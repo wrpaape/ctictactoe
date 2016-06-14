9aaf895bce486cf57ade0bd9f2b623f643129d85
Reid Paape <wrpaape@gmail.com>
2016-06-13 19:12:09 -0500

    need to decouple dims

--- src/tictactoe_display.c
+++ src/tictactoe_display.c
@@ -1,25 +1,84 @@
 #include "tictactoe_display.h"
 
-void init_display(struct *const restrict Display display,
+void display_init(struct *const restrict Display display,
 		  const unsigned int *input_move_map,
 		  const unsigned int cell_count)
 {
 	init_display_dims(&display->dims,
 			  cell_count);
 
-	HANDLE_MALLOC(display->cell_map,
+	HANDLE_MALLOC(display->cells,
 		      sizeof(struct DisplayBoardCell *) * cell_count);
+}
 
+void dims_init(struct *const restrict DisplayDims dims,
+	       const unsigned int cell_count)
+{
+	struct winsize window;
+
+	get_stdio_winsize(&window);
+
+	ratio_pair_set(&board->pad_total,
+		       0.2,
+		       0.1);
+
+	board_spec_set(board,
+		       &window);
+
+	ratio_pair_set(&cell->pad_total,
+		       0.1,
+		       0.1);
+
+	dims->cell_count = cell_count;
+}
+
+inline void board_spec_set(struct *const restrict DisplaySpec board,
+			   const struct *const restrict winsize window)
+{
+	dim_pair_set(&board->total,
+		     (unsigned int) window->ws_col,
+		     (unsigned int) window->ws_row);
+
+	dim_spec_set_pad_fill(board);
 
 }
 
-void init_display_dims(struct *const restrict DisplayDims dims,
-		       const unsigned int cell_count)
+inline void dim_spec_set_pad_fill(struct *const restrict DimSpec spec)
 {
-	dims->cell_count = cell_count;
+	dim_pair_set(&spec->pad,
+		     spec->total.x * spec->pad_total.x,
+		     spec->total.y * spec->pad_total.y);
 
-	get_stdio_winsize(&dims->window);
+	dim_pair_set(&spec->fill,
+		     spec->total.x - spec->pad.x,
+		     spec->total.y - spec->pad.y);
+}
 
-	dims->window.ws_row;
-	dims->window.ws_col;
+
+
+inline void cell_total_set(struct *const restrict DimPair cell_total,
+			   struct *const restrict DimPair board_fill,
+			   const unsigned int cell_count)
+{
+	const unsigned int lines_per_dim = cell_count + 1u;
+
+	cell_total->x = (board_fill->x - lines_per_dim) / cell_count;
+	cell_total->y = (board_fill->y - lines_per_dim) / cell_count;
+}
+
+
+inline void dim_pair_set(struct *const restrict DimPair dims,
+			 const unsigned int x,
+			 const unsigned int y)
+{
+	dims->x = x;
+	dims->y = y;
+}
+
+inline void ratio_pair_set(struct *const restrict RatioTuple ratios,
+			   const double x,
+			   const double y)
+{
+	ratios->x = x;
+	ratios->y = y;
 }
--- src/tictactoe_display.h
+++ src/tictactoe_display.h
@@ -19,83 +19,6 @@ extern "C" {
  * EXTERNAL DEPENDENCIES
  *
  *
- * TYPEDEFS, ENUM AND STRUCT DEFINITIONS
- * ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
-
-struct DisplayBoardLineJoin {
-	const char *left;			/* ├┌└ */
-	const char *center;			/* ┼ characters */
-	const char *right;			/* ┐┤┘ characters */
-};
-
-
-
-
-┬
-┴
-
-
-─
-│
-┼
-
-
-struct DisplayBoardLines {
-	const char *segment;			/* ───... */
-	struct DisplayBoardLineJoin top;
-	struct DisplayBoardLineJoin mid;
-	struct DisplayBoardLineJoin bot;
-};
-
-struct DisplayBoardCell {
-	bool is_marked;
-	struct TokenMark mark;			/* mark or placeholder */
-};
-
-struct DisplayBoardBuffer {
-	char *complete;	/* start of entire string */
-	char *active;	/* start of unsettled portion */
-};
-
-struct DisplayBoard {
-	struct DisplayBoardGuide guide;		/* string building template */
-	struct DisplayBoardBuffer buffer;	/* string representation */
-};
-
-struct DisplayDimTuple {
-	unsigned int x;
-	unsigned int y;
-};
-
-struct DisplayRatioTuple {
-	double x;
-	double y;
-};
-
-struct DisplayContainerDims {
-	struct DisplayDimTuple pad;
-	struct DisplayDimTuple fill;
-	struct DisplayRatioTuple pad_to_fill;
-};
-
-
-struct DisplayDims {
-	unsigned int cell_count;		/* config on setup */
-	struct winsize window;			/* stateful */
-	struct DisplayContainerDims board;	/* stateful */
-	struct DisplayContainerDims cell;	/* stateful */
-}
-
-struct Display {
-	struct DisplayDims dims;	/* stateful */
-	struct DisplayBoard board;	/* stateful */
-	struct DisplayBoardCell **map;	/* config on setup */
-};
-
-/* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
- * TYPEDEFS, ENUM AND STRUCT DEFINITIONS
- *
- *
  * CONSTANTS
  * ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
 
@@ -103,9 +26,76 @@ struct Display {
 
 #define MAX_ROWS 54ul
 #define MAX_COLS 181ul
+#define MAX_DISPLAY_SIZE (MAX_ROWS * MAX_COLS * UTF8_MAX_SIZE)
 
 /* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
  * CONSTANTS
+ *
+ *
+ * TYPEDEFS, ENUM AND STRUCT DEFINITIONS
+ * ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+
+struct DisplayLineJoins {
+	const char *left;			/* ┌ ├ └ */
+	const char *center;			/* ┬ ┼ ┴ */
+	const char *right;			/* ┐ ┤ ┘ */
+};
+
+struct DisplayLineSpec {
+	const char *horiz;
+	const char *vert;
+	struct DisplayLineJoins top;
+	struct DisplayLineJoins mid;
+	struct DisplayLineJoins bot;
+};
+
+struct DisplayBoardCell {
+	bool is_marked;
+	struct TokenMark *mark;	/* mark or placeholder */
+};
+
+struct DisplayBuffer {
+	char complete[MAX_DISPLAY_SIZE];
+	char *active;	/* start of unsettled portion */
+};
+
+struct DisplayString {
+	struct DisplayBuffer buffer;		/* string representation */
+	struct DisplayBoardCell *cells;		/* stateful */
+	struct DisplayLineSpec line_pieces;	/* config on setup */
+};
+
+struct DimPair {
+	unsigned int x;
+	unsigned int y;
+};
+
+struct RatioPair {
+	double x;
+	double y;
+};
+
+struct DimSpec {
+	struct DimPair pad;
+	struct DimPair fill;
+	struct DimPair total;
+	struct RatioPair pad_total;	/* pad / total */
+};
+
+
+struct DisplayDims {
+	unsigned int cell_count;		/* config on setup */
+	struct DimSpec board;	/* stateful */
+	struct DimSpec cell;	/* stateful */
+};
+
+struct Display {
+	struct DisplayDims dims;		/* stateful */
+	struct DisplayString string;		/* stateful */
+};
+
+/* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
+ * TYPEDEFS, ENUM AND STRUCT DEFINITIONS
  *
  *
  * FUNCTION-LIKE MACROS
@@ -117,10 +107,9 @@ struct Display {
  * TOP-LEVEL FUNCTIONS
  * ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
 
-void init_display(struct *const restrict Display display);
+void display_init(struct *const restrict Display display);
 
-void init_display_dims(struct *const restrict DisplayDims dims,
-		       const unsigned int cell_count);
+
 
 /* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
  * TOP-LEVEL FUNCTIONS
@@ -128,6 +117,28 @@ void init_display_dims(struct *const restrict DisplayDims dims,
  *
  * HELPER FUNCTIONS
  * ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+
+static void dims_init(struct *const restrict DisplayDims dims,
+		      const unsigned int cell_count);
+
+static inline void dim_spec_set_pad_fill(struct *const restrict DimSpec spec);
+
+
+static inline void board_spec_set(struct *const restrict DisplaySpec board,
+				   const struct *const restrict winsize window);
+
+static inline void dim_pair_set(struct *const restrict DimPair dims,
+				const unsigned int x,
+				const unsigned int y);
+
+static inline void ratio_pair_set(struct *const restrict RatioPair ratios,
+				  const double x,
+				  const double y);
+
+static inline void cell_total_set(struct *const restrict DimPair cell_total,
+				  struct *const restrict DimPair board_total,
+				  const unsigned int cell_count);
+
 /* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
  * HELPER FUNCTIONS */
 
